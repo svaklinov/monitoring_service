97a8e2968dd374da696d6c2dbbc9a241ade58e04
nem-svaklinov <svaklinov@nemetschek.bg>
2024-07-26 11:21:49 +0300

    change slim image with alpine in Dockerfile

--- IaC/2-vm.tf
+++ IaC/2-vm.tf
@@ -4,6 +4,7 @@ resource "aws_instance" "this" {
   instance_type   = "t3.medium"
   subnet_id       = module.vpc.public_subnets[0]
   security_groups = [aws_security_group.http_access.id]
+  disable_api_termination = true # Prevent AWS API destroy accidently EC2 instance  - recommended from Snyck
 
   tags = {
     Name = var.vm_name
